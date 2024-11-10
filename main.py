64f7318264966cadc53522d03a9bbd67174b2dd1
Spidy394 <mondalshubhodeep30924@gmail.com>
2024-11-10 20:56:16 +0530

    reset

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ import discord
 from discord.ext import commands 
 import os
 from dotenv import load_dotenv
+import pytz
 
 
 load_dotenv()
 token = os.getenv('Token')
-
 if token is None:
     raise ValueError("No token found in environment variables. Make sure 'Token' is set in your .env file.")
 
+local_timezone = pytz.timezone("Asia/Kolkata")
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print("ByteMe in now online")
     
-    from funx.hello import on_message
-    from funx.pong import on_message
+    async def on_message(self, message):
+        print(f"Message from \"{message.author}\" => \"{message.content}\" in \"{message.channel}\" at {message.created_at.astimezone(local_timezone)} of \"{message.guild}\"")
+        if message.author == self.user:
+            return
+        
+        
+
 
 intents = discord.Intents.default()
 intents.message_content = True
